ad54ac446e0672a0fa8b4ae51b9833cb13038002
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-20 12:22:45 +0200

    added scene

---
+++ game.cpp
@@ -0,0 +1,57 @@
+#include "game.h"
+#include <QGraphicsTextItem>
+#include <QFont>
+#include <QTimer>
+
+#include <QApplication>
+
+//#define IOS
+
+
+Game::Game() :
+    m_scene{nullptr},
+    m_quit{nullptr},
+    m_music{nullptr}
+{
+    // Building the scene
+    m_scene = new QGraphicsScene();
+    setScene(m_scene);
+
+    // Create exit button thet lives with the scene
+    m_quit = new QPushButton("Quit");
+    m_quit->setGeometry(m_scene->width()-100,0,100,50);
+    m_quit->setStyleSheet("QPushButton { background-color: yellow; }");
+    m_scene->addWidget(m_quit);
+
+    QObject::connect(m_quit, &QPushButton::clicked, qApp, &QApplication::quit);
+
+
+
+}
+
+Game::~Game()
+{
+    //1
+    if(m_quit)
+    {
+        delete m_quit;
+        m_quit = nullptr;
+    }
+    //2
+    if(m_music)
+    {
+        delete m_music;
+        m_music = nullptr;
+    }
+    //3
+    if(m_scene)
+    {
+        delete m_scene;
+        m_scene = nullptr;
+    }
+}
+
+void Game::stop()
+{
+
+}
--- shooter-game-qt5.pro
+++ shooter-game-qt5.pro
@@ -1,4 +1,4 @@
-QT       += core gui
+QT       += core gui multimedia
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -9,9 +9,11 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    game.cpp \
     main.cpp
 
-HEADERS +=
+HEADERS += \
+    game.h
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
